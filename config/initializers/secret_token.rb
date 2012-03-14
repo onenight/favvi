f759d0e4b472d570e4ea0b10ec7f1b87ae11ca65
onenight <artonster7@gmail.com>
2012-03-14 14:43:27 +0800

    favvi start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Favvi::Application.config.secret_token = '14d10a67f735afe724f1fa342c17801b1fce08256dd98ef4e8b151660599527a0b9862a738784c68b2c488d974ffc918c04b315bb057a7edacc5b31763619b47'
