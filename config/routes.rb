e841f061d8990b1148128431e1c8283e87bb0b01
onenight <artonster7@gmail.com>
2012-03-20 15:18:55 +0800

    add user login

--- config/routes.rb
+++ config/routes.rb
@@ -62,7 +62,7 @@ Favvi::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'brands#index'
 
   # See how all your routes lay out with "rake routes"
 
