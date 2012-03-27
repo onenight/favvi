67093c7aecbb9a6683dd0d2d1ab412d9c7e310ef
onenight <artonster7@gmail.com>
2012-03-28 01:20:15 +0800

    create home model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120325110053) do
+ActiveRecord::Schema.define(:version => 20120327171947) do
 
   create_table "angels", :force => true do |t|
     t.string   "name"
@@ -39,6 +39,13 @@ ActiveRecord::Schema.define(:version => 20120325110053) do
     t.datetime "updated_at"
     t.string   "image"
     t.string   "categories"
+  end
+
+  create_table "homes", :force => true do |t|
+    t.string   "name"
+    t.string   "image"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "infos", :force => true do |t|
