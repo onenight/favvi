6e4812568ff3a4921bc7b3fab2d3c6b64d07541e
onenight <artonster7@gmail.com>
2012-03-28 23:25:45 +0800

    create photo model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120327171947) do
+ActiveRecord::Schema.define(:version => 20120328151107) do
 
   create_table "angels", :force => true do |t|
     t.string   "name"
@@ -52,6 +52,14 @@ ActiveRecord::Schema.define(:version => 20120327171947) do
     t.string   "title"
     t.text     "content"
     t.string   "image"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "photos", :force => true do |t|
+    t.string   "name"
+    t.integer  "imageable_id"
+    t.string   "imageable_type"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
